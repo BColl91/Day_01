b38a2b12559f90b69c69b1b7b96c79eec513f364
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-01 18:28:05 +0100

    Save_01

---
+++ Activity.js
@@ -0,0 +1,24 @@
+// let i = 10;
+// i = i + 2;
+//Below is an alternative method that will bring the same outcome: =12
+// i += 2;
+
+
+// let favouriteDrink = "coffee";
+// console.log(favouriteDrink);
+
+// let favouriteDrink = "coffee";
+// console.log("My favourite drink is " + favouriteDrink);
+
+//Activity 1
+let name = "Bex";
+let age = 32;
+let favColor = "Purple";
+
+console.log(`Hi my name is ${name}, I am ${age} and my favourite colour is ${favColor}. `)
+
+name = "G";
+age = "33";
+favColor = "Blue";
+
+console.log(`Hi my name is ${name}, I am ${age} and my favourite colour is ${favColor}. `)
---
+++ dotnotation.js
@@ -0,0 +1,8 @@
+// console.log("hello world".toUpperCase());
+// console.log("hello world".toLowerCase());
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10));
+// console.log(Math.ceil(Math.random()*10));
+// console.log(Math.round(Math.random()*10));
+
